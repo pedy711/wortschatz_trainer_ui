d864197fc90c84a059929fe456e08605ddb76a59
milandis <mzbineshfar@gmail.com>
2020-04-26 16:42:43 +0200

    i changed constants.dart

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -13,44 +13,43 @@ class Constants{
 
 
   static final String APP_NAME_ENGLISH = "Hamrah";
-  static final String APP_NAME_PERSIAN = "همراه";
+  static final String APP_NAME_PERSIAN = "Mobile";
   static final int    APP_VERSION = 1;
-  static final String GET_STARTED_BTN = "بزن بریم";
+  static final String GET_STARTED_BTN = "Let's Go";
   static final String BECOME_A_MEMBER = "Mit email anmelden";
+  static final String START_TO_READ = "START";
   static final String EMAIL_TEXT = "Email";
   static final String PASSWORD_TEXT = "Password";
   static final String REGISTER = "Anmelden";
   static final String REGISTER_OR_LOGIN = "Melde dich an oder einloggen";
   static final String LOGIN_WITH_FACEBOOK = "Mit Facebook anmelden";
-  static final String REPEAT_PASSWORD_TEXT = "تکرار کلمه عبور";
+  static final String REPEAT_PASSWORD_TEXT = "Repeat Password";
   static final String LOGIN = "Einloggen";
   static final String ALREADY_A_MEMBER_TEXT = "Bist du schon mitglied?";
-  static final String NEXT_TEXT = "بعدی";
+  static final String NEXT_TEXT = "Next";
   static final String SIGN_UP = "Sign Up";
-  static final String SELF_SUMMARY_TITLE_TEXT = "راجع به خودت بگو";
+  static final String SELF_SUMMARY_TITLE_TEXT = "Tell about yourself";
   static final String SELF_SUMMARY_TEXT = "که ما بتونیم افرادی که دوست داری برات پیدا کنیم.";
-  static final String FIRST_NAME_TEXT = "نام کوچک";
-  static final String LAST_NAMENAME_TEXT = "نام خانوادگی";
-  static final String ABOUT_YOU_TEXT = "درباره تو";
+  static final String FIRST_NAME_TEXT = "Name";
+  static final String LAST_NAMENAME_TEXT = "Family ";
+  static final String ABOUT_YOU_TEXT = "About You ";
   static final String TODAY_WORD_LIST = "Die neuen Wörter des Tages";
 
   // Introduction Page
-  static final String FEMALE = "زن                                                          👩";
-  static final String MALE = "مرد                                                         🧑";
+  static final String FEMALE = "Woman                                                          👩";
+  static final String MALE = "Man                                                         🧑";
 /*  static final String FEMALE = "زن 👩";
   static final String MALE = "مرد 🧑";*/
-  static final String BIRTHDAY = "روز تولد";
+  static final String BIRTHDAY = "Birthday ";
   static final String OK = "تائید";
   static final String CANCEL = "انصراف";
-  static final String WHERE_DO_YOU_LIVE = "کجا زندگی می کنی؟";
+  static final String WHERE_DO_YOU_LIVE = "Where do you live?";
   static final String AUTOMATIC_LOCATION = "موقعیت مکانی خودت رو به صورت خودکار پر کن";
   static final String ACTIVATE = "فعال کن";
   static final String ADD_SOME_GREAT_PHOTOS = "چند تا عکس عالی اضافه کن";
   static final String SHORT_SUMMARY = "خودت رو به طور خلاصه معرفی کن";
   static final String SHORT_SUMMARY_Label = "برای مثال، بهترین دوستت چه طور توصیفت می کنه؟";
 
-  // words overview page
-  static final String START = "Start";
 
 
 
